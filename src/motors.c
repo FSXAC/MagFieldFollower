60cd427515a5d599fb8db3727b6244120ee7002a
larrylikestocode <yunpengliu1997@126.com>
2017-03-23 10:59:06 -0700

    update
    Moving on to check build in pwm

--- src/motors.c
+++ src/motors.c
@@ -10,10 +10,17 @@
 #define SYSCLK    48000000L // SYSCLK frequency in Hz
 #define BAUDRATE  115200L   // Baud rate of UART in bps
 
-#define OUT0 P2_0
-#define OUT1 P2_1
+#define MOTOR_LEFT0 P2_1
+#define MOTOR_LEFT1 p2_2
+#define MOTOR_RIGHT0 P2_3
+#define MOTOR_RIGHT1 P2_3
 
 volatile unsigned char pwm_count=0;
+volatile int mode = 0;
+volatile int pwm_Left = 0;
+volatile int pwm_right = 0;
+volatile int pwm_both = 0;
+volatile int direction = 0; // 1 for back 0 for forward
 
 char _c51_external_startup (void)
 {
@@ -23,7 +30,7 @@ char _c51_external_startup (void)
 
 	// CLKSEL&=0b_1111_1000; // Not needed because CLKSEL==0 after reset
 	#if (SYSCLK == 12000000L)
-		//CLKSEL|=0b_0000_0000;  // SYSCLK derived from the Internal High-Frequency Oscillator / 4 
+		//CLKSEL|=0b_0000_0000;  // SYSCLK derived from the Internal High-Frequency Oscillator / 4
 	#elif (SYSCLK == 24000000L)
 		CLKSEL|=0b_0000_0010; // SYSCLK derived from the Internal High-Frequency Oscillator / 2.
 	#elif (SYSCLK == 48000000L)
@@ -34,14 +41,14 @@ char _c51_external_startup (void)
 	OSCICN |= 0x03; // Configure internal oscillator for its maximum frequency
 
 	// Configure UART0
-	SCON0 = 0x10; 
+	SCON0 = 0x10;
 #if (SYSCLK/BAUDRATE/2L/256L < 1)
 	TH1 = 0x10000-((SYSCLK/BAUDRATE)/2L);
 	CKCON &= ~0x0B;                  // T1M = 1; SCA1:0 = xx
 	CKCON |=  0x08;
 #elif (SYSCLK/BAUDRATE/2L/256L < 4)
 	TH1 = 0x10000-(SYSCLK/BAUDRATE/2L/4L);
-	CKCON &= ~0x0B; // T1M = 0; SCA1:0 = 01                  
+	CKCON &= ~0x0B; // T1M = 0; SCA1:0 = 01
 	CKCON |=  0x01;
 #elif (SYSCLK/BAUDRATE/2L/256L < 12)
 	TH1 = 0x10000-(SYSCLK/BAUDRATE/2L/12L);
@@ -53,14 +60,14 @@ char _c51_external_startup (void)
 #endif
 	TL1 = TH1;      // Init Timer1
 	TMOD &= ~0xf0;  // TMOD: timer 1 in 8-bit autoreload
-	TMOD |=  0x20;                       
+	TMOD |=  0x20;
 	TR1 = 1; // START Timer1
 	TI = 1;  // Indicate TX0 ready
-	
+
 	// Configure the pins used for square output
 	P2MDOUT|=0b_0000_0011;
 	P0MDOUT |= 0x10; // Enable UTX as push-pull output
-	XBR0     = 0x01; // Enable UART on P0.4(TX) and P0.5(RX)                     
+	XBR0     = 0x01; // Enable UART on P0.4(TX) and P0.5(RX)
 	XBR1     = 0x40; // Enable crossbar and weak pull-ups
 
 	// Initialize timer 2 for periodic interrupts
@@ -72,7 +79,7 @@ char _c51_external_startup (void)
 	TR2=1;         // Start Timer2
 
 	EA=1; // Enable interrupts
-	
+
 	return 0;
 }
 
@@ -80,21 +87,39 @@ char _c51_external_startup (void)
 void Timer2_ISR (void) interrupt 5
 {
 	TF2H = 0; // Clear Timer2 interrupt flag
-	
+
 	pwm_count++;
 	if(pwm_count>100) pwm_count=0;
-	
+
 	OUT0=pwm_count>50?0:1;
 	OUT1=pwm_count>75?0:1;
 }
+
+void forward_backward(int pwm_both, int direction)
+{
+	(direction == 0) ? (MOTOR_LEFT0 = 0; MOTOR_RIGHT0 = 0;) :  
+
+}
+
 
 void main (void)
 {
 	printf("\x1b[2J"); // Clear screen using ANSI escape sequence.
 	printf("Square wave generator for the F38x.\r\n"
-	       "Check pins P2.0 and P2.1 with the oscilloscope.\r\n");
+	       "Check pins P2.1 and P2.2 with the oscilloscope.\r\n");
+	printf("Please enter motors mode 1-6");
+	scanf("%d \n",&mode);
+	if(mode == 1) {printf("Enter pwm and direction"); scanf("%d %d",pwm_both, direction); }
+
 
 	while(1)
 	{
+		switch(mode)
+		{
+			//forward_backward mode
+			case mode == 1: forward_backward(pwm_both, direction);
+		}
+
+
 	}
 }
